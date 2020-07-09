67190ee71fa7ce9113a352363bfc2b89670319e6
Nauman Munir <naumanmunir@gmail.com>
2020-07-10 00:06:22 +0500

    Banner component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export class Error extends Component {
   render() {
     return (
-      <div>
-        Hello from Error Page
-      </div>
-    )
+      <Hero>
+        <Banner title="404" subtitle="page not found">
+          <Link to='/' className='btn-primary' >
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
 
-export default Error
+export default Error;
