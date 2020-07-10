f35d758a307e046a0579f58b36069a490ec859d2
Nauman Munir <naumanmunir@gmail.com>
2020-07-10 17:20:47 +0500

    componentDidMount

--- src/context.js
+++ src/context.js
@@ -1,11 +1,33 @@
 import React, { Component, createContext } from "react";
+import items from "./data";
 
 const RoomContext = createContext();
 
 class RoomProvider extends Component {
   state = {
-    
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
   };
+
+  // Get Data
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
 
   render() {
     return (
