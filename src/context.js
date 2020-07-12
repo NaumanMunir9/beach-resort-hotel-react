f4f049798a7c8bc488760789dd708fe60899ebf4
Nauman Munir <naumanmunir@gmail.com>
2020-07-12 19:05:37 +0500

    context and RoomFilter

--- src/context.js
+++ src/context.js
@@ -57,7 +57,7 @@ class RoomProvider extends Component {
 
   handleChange = (e) => {
     const target = e.target.type;
-    const value = e.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = e.target.name;
     this.setState(
       {
@@ -79,11 +79,42 @@ class RoomProvider extends Component {
       pets,
     } = this.state;
 
+    // all the rooms
     let tempRooms = [...rooms];
+
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    // filter by type
     if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
 
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    // filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms,
     });
